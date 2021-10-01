bcece6ee805d29cdd3e1247c6451cbb11e4e7ac0
Artem <artmrsvch@gmail.com>
2021-10-01 14:57:32 +0300

    chore: delete unused constans

--- src/Draggable/settigs.js
+++ src/Draggable/settigs.js
@@ -2,59 +2,6 @@ import { HeaderOne, HeaderTwo, HeaderThree } from "./Headers/index";
 import { MainOne, MainTwo, MainThree } from "./Mains/index";
 import { FooterOne, FooterTwo, FooterThree } from "./Footers/index";
 
-export const category = [
-  {
-    name: "Header",
-    presents: [
-      {
-        subName: "HeaderOne",
-        component: HeaderOne
-      },
-      {
-        subName: "HeaderTwo",
-        component: HeaderTwo
-      },
-      {
-        subName: "HeaderThree",
-        component: HeaderThree
-      }
-    ]
-  },
-  {
-    name: "Main",
-    presents: [
-      {
-        subName: "MainOne",
-        component: MainOne
-      },
-      {
-        subName: "MainTwo",
-        component: MainTwo
-      },
-      {
-        subName: "MainThree",
-        component: MainThree
-      }
-    ]
-  },
-  {
-    name: "Footer",
-    presents: [
-      {
-        subName: "FooterOne",
-        component: FooterOne
-      },
-      {
-        subName: "FooterTwo",
-        component: FooterTwo
-      },
-      {
-        subName: "FooterThree",
-        component: FooterThree
-      }
-    ]
-  }
-];
 export const lib = {
   HeaderOne: {
     id: 1,
